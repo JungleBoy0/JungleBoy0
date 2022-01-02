31be89e950a09083e805dd60597a7e0e28083920
JungleBoy0 <90778929+JungleBoy0@users.noreply.github.com>
2022-01-02 01:44:54 +0100

    Add files via upload

---
+++ es/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LogiGen - JungleBoy#0269</title>
+  
+    <style><?php include 'D:\Programowanie\XAMPP\htdocs\GeneratorSite\css\style.css'; ?></style>
+</head>
+<body id="all">
+
+<!-- If you reading this FUCK YOU, write your own code and yes i know that code suck :D-->
+<?php 
+$A86EBF399F6C5F112D8984AC32AD2 = "BxCc5Uhvfe9sAGF8jyPWfquR9Q3VjAF0";
+$UcqKspwDZt = "2002LZ0F";
+$kf8 = "8";
+
+
+?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+<p class="unbroken"></p>
+<p id="broken" class="broken" style="color: #242424;"></p>
+<script>
+  function generate1(){
+$.ajax({
+  method: "POST",
+  url: "wrap.php",
+  data: { text: $("p.unbroken").text() }
+})
+  .done(function( response ) {
+    $("p.broken").html(response);
+  });
+}
+</script>
+<script type="text/javascript">
+
+var od40rt0EyOo5Xyc830jyIzBl5xPnp2aT = false;
+var nmQk4g72hGdA97aFdRJ6vUhwYkxv2Or7 = <?php echo json_encode($A86EBF399F6C5F112D8984AC32AD2); ?>;
+var Y0ZQ0QX1qY95H38Pw1DiFecKJPHp81Za = <?php echo json_encode($UcqKspwDZt); ?>;
+var kf38 = <?php echo json_encode($kf8); ?>;
+
+
+function keycheck() {
+  var AD45D1E9CA3B9C2D531DFF585DC5D = document.getElementById("nmQk4g72hGdA97aFdRJ6vUhwYkxv2Or7");
+if (AD45D1E9CA3B9C2D531DFF585DC5D.value == nmQk4g72hGdA97aFdRJ6vUhwYkxv2Or7){
+  AD45D1E9CA3B9C2D531DFF585DC5D.value = "Activated"
+  od40rt0EyOo5Xyc830jyIzBl5xPnp2aT = true;
+}
+else
+AD45D1E9CA3B9C2D531DFF585DC5D.value = "Wrong Key";
+
+}
+ <!-- -->
+
+
+
+
+function generate(){
+  var cbBp0xTEK8ImXXT0uIK4JWIoQrHyhCN = document.getElementById("broken").innerHTML;
+    var SgAQ32YO9oCOveo78Sv7M6LBREzywJE0 = document.getElementById("SgAQ32YO9oCOveo78Sv7M6LBREzywJE0");
+    if (od40rt0EyOo5Xyc830jyIzBl5xPnp2aT == true){
+      SgAQ32YO9oCOveo78Sv7M6LBREzywJE0.textContent = Y0ZQ0QX1qY95H38Pw1DiFecKJPHp81Za + cbBp0xTEK8ImXXT0uIK4JWIoQrHyhCN + kf38;
+    }
+    else
+    SgAQ32YO9oCOveo78Sv7M6LBREzywJE0.textContent = "(FIRST USE YOUR ACTIVATION KEY)";
+}
+
+
+
+
+</script>
+ 
+
+
+
+
+<!-- All -->
+<div id="screen">
+    <div id="logocss">
+        <span id="logo"> ("# LogiGen by JungleBoy#0269# ");<br/>
+            ("# Free VERISON # ");<br/></span>
+           
+    </div>
+    <div id="serialcss">
+    <span id="SgAQ32YO9oCOveo78Sv7M6LBREzywJE0">(Your Serial)</span>
+</div>
+    <div style="text-align: center;">
+        <span"><a href="https://www.nulled.to/user/4909186-jungleboy0">Nulled.to Profile</a></span>
+    </div>
+    <div class="input">
+
+        <input id="nmQk4g72hGdA97aFdRJ6vUhwYkxv2Or7"   type=text placeholder="Activation Key">
+
+    </div>
+    <div style="text-align: center;">
+    <button class="button" name="button" style="margin-bottom: 0.5%; margin-left: 0.5%;" onclick="generate1(); generate();">Generate</button>
+    <button class="button" onclick="keycheck()">Check Key</button>
+    </div>
+    
+    <!-- Full Version -->
+<div id="down">
+    Full version:<br>
+    <video width="600" height="338" controls style="border: 5px solid black;">
+        <source src="images/trailer.mp4" type="video/mp4">
+      Your browser does not support the video tag.
+      </video>
+
+</div>
+</div>
+
+
+
+</body>
+</html>
